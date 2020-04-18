16ea945aa9750e17f452edf65043adc433e5568c
tuananh8497 <tuananh8497@gmail.com>
2020-04-18 22:23:30 +0700

    First Commit (test)

---
+++ ST2_Assgt2/src/StackQuestion.java
@@ -0,0 +1,20 @@
+import java.util.Stack;
+
+public class StackQuestion {
+  public static void main(String[] args) {
+    Stack stk = new Stack<>();
+
+    stk.push(17);
+    stk.push(5);
+    stk.push(10);
+    stk.push(15);
+    stk.push(9);
+    System.out.println(stk);
+  }
+
+  // TODO Complete the sortStack method to sort the input stack
+  public static Stack<Integer> sortStack(Stack<Integer> input) {
+    // Add your code here to sort Stacks such that
+    // the smallest items are on the top
+  }
+}
