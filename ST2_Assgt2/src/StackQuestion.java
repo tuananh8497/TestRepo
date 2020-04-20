b56f1c59c7af5fa7d3f9ba5d75b14971264882f9
tuananh8497 <tuananh8497@gmail.com>
2020-04-20 20:50:04 +0700

    Added random number generator

--- ST2_Assgt2/src/StackQuestion.java
+++ ST2_Assgt2/src/StackQuestion.java
@@ -1,21 +1,30 @@
+import java.util.Random;
 import java.util.Stack;
 
 public class StackQuestion {
   public static void main(String[] args) {
     Stack stk = new Stack<>();
+    Random r = new Random();
 
-    stk.push(17);
-    stk.push(5);
-    stk.push(10);
-    stk.push(15);
-    stk.push(9);
-    System.out.println(stk);
-    System.out.println("Hello");
+    for (int i = 0; i < 5; i++) {
+      int x = r.nextInt(100);
+      stk.push(x);
+      System.out.println("loop #" + i + ": " + stk.peek());
+    }
+    System.out.println("Before sort: " + stk);
+    sortStack(stk);
+    System.out.println("After sort: " + stk);
   }
 
   // TODO Complete the sortStack method to sort the input stack
   public static Stack<Integer> sortStack(Stack<Integer> input) {
-    // Add your code here to sort Stacks such that
-    // the smallest items are on the top
+
+    Stack tmp = new Stack<>();
+    if (tmp.empty()) {
+      tmp.push(input.pop());
+    }
+
+
+    return input;
   }
 }
